52ee8741f613896d8f6a44ec6eceda6f3d1e41a0
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-27 17:29:33 -0400

    Query for rollover students

---
+++ 1_queries/1_rollover_students.sql
@@ -0,0 +1,4 @@
+SELECT students.name as student_name, students.start_date as student_start_date, cohorts.name as cohort_name, cohorts.start_date as cohort_start
+FROM students INNER JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_starts; 
